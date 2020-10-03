f70627d29fb60e3a29b52e055bea13c2f74f282e
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-03 16:43:38 -0700

    Reaect Initial setup

---
+++ c-tracker-dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Covid tracker dashboard</h1>
+
+      {/* Header */}
+      {/* Title */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
