130aea65b1feabf8913b05648d3810afd171dc39
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-25 23:42:57 -0700

    deploy setup in github

---
+++ build/precache-manifest.cf6eea14856bfb2b1039fb9aac49ad57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3fbba69591b97ee0039a8328369823",
+    "url": "/cTrackerDashboard/index.html"
+  },
+  {
+    "revision": "0efa3fb4bc712054661b",
+    "url": "/cTrackerDashboard/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "5b02cd5c72a221cc598b",
+    "url": "/cTrackerDashboard/static/css/main.0ed6ee53.chunk.css"
+  },
+  {
+    "revision": "0efa3fb4bc712054661b",
+    "url": "/cTrackerDashboard/static/js/2.e864b44a.chunk.js"
+  },
+  {
+    "revision": "4ee702cb7d7cdd2166a72cd76464a5ce",
+    "url": "/cTrackerDashboard/static/js/2.e864b44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b02cd5c72a221cc598b",
+    "url": "/cTrackerDashboard/static/js/main.857da817.chunk.js"
+  },
+  {
+    "revision": "5ba369242d90c1b67ca8",
+    "url": "/cTrackerDashboard/static/js/runtime-main.25d89562.js"
+  }
+]);
