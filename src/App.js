148c8bf608a3784a6e67e12978dde7ef830919a0
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-25 23:33:01 -0700

    Increase chart height

--- src/App.js
+++ src/App.js
@@ -96,17 +96,21 @@ function App() {
 
       <div className="app__stats">
         <InfoBox
+        isRed
         active={casesType === "cases"}
         onClick={e => setCasesType('cases')}
           title="Coronavirus Cases"
           cases={prettyPrintStat(countryInfo.todayCases)}
           total={prettyPrintStat(countryInfo.cases)} />
         <InfoBox
+        active={casesType === "recovered"}
         onClick={e => setCasesType('recovered')}
           title="Recovered"
           cases={prettyPrintStat(countryInfo.todayRecovered)}
           total={prettyPrintStat(countryInfo.recovered)}/>
         <InfoBox
+        isRed
+        active={casesType === "deaths"}
         onClick={e => setCasesType('deaths')}
           title="Deaths"
           cases={prettyPrintStat(countryInfo.todayDeaths)}
@@ -126,9 +130,9 @@ function App() {
           <h3>Live Cases by Country</h3>
             {/* Table */}
           <Table countries={tableData}/>
-            <h3>Worldwide new {casesType}</h3>
+            <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
            {/* Graph */}
-          <LineGraph casesType={casesType}/>
+          <LineGraph className="app__graph" casesType={casesType}/>
 
         </CardContent>
       </Card>
--- src/InfoBox.js
+++ src/InfoBox.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import './styles/InfoBox.css';
 
-function InfoBox({ title, cases, active, total, ...props }) {
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
 
   return (
     <Card onClick={props.onClick}
-    className={`infoBox`}>
+    className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}>
       <CardContent className='infobox-container'>
         {/* Title i.e. Coronavirus cases */}
         <Typography className="infoBox__title" color="textSecondary">
@@ -15,7 +15,7 @@ function InfoBox({ title, cases, active, total, ...props }) {
         </Typography>
 
         {/* +120k Number of cases */}
-        <h2 className="infoBox__cases">{cases}</h2>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--blue"}`}>{cases}</h2>
 
         {/* +1.2M Total */}
         <Typography className="infoBox__total" color="textSecondary">
